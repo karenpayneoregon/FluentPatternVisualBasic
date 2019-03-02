05594959660f9a8831d2c019462193d6d42fe05a
Karen Payne <paynekaren@comcast.net>
2019-03-01 16:40:52 -0800

    Changed tab order

--- NorthWindCustomers/Form1.Designer.vb
+++ NorthWindCustomers/Form1.Designer.vb
@@ -24,17 +24,17 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.columnNamesComboBox = New System.Windows.Forms.ComboBox()
-        Me.decendingOrderCheckBox = New System.Windows.Forms.CheckBox()
-        Me.readCustomersButton = New System.Windows.Forms.Button()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.readCustomersByCountryButton = New System.Windows.Forms.Button()
         Me.countriesComboBox = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.readCustomersButton = New System.Windows.Forms.Button()
+        Me.decendingOrderCheckBox = New System.Windows.Forms.CheckBox()
+        Me.columnNamesComboBox = New System.Windows.Forms.ComboBox()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
-        Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'DataGridView1
@@ -43,7 +43,7 @@ Partial Class Form1
         Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(800, 450)
+        Me.DataGridView1.Size = New System.Drawing.Size(800, 345)
         Me.DataGridView1.TabIndex = 0
         '
         'Panel1
@@ -55,46 +55,6 @@ Partial Class Form1
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(800, 105)
         Me.Panel1.TabIndex = 1
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.readCustomersButton)
-        Me.GroupBox1.Controls.Add(Me.decendingOrderCheckBox)
-        Me.GroupBox1.Controls.Add(Me.columnNamesComboBox)
-        Me.GroupBox1.Location = New System.Drawing.Point(9, 10)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(282, 84)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "Order by"
-        '
-        'columnNamesComboBox
-        '
-        Me.columnNamesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.columnNamesComboBox.FormattingEnabled = True
-        Me.columnNamesComboBox.Location = New System.Drawing.Point(9, 23)
-        Me.columnNamesComboBox.Name = "columnNamesComboBox"
-        Me.columnNamesComboBox.Size = New System.Drawing.Size(152, 21)
-        Me.columnNamesComboBox.TabIndex = 0
-        '
-        'decendingOrderCheckBox
-        '
-        Me.decendingOrderCheckBox.AutoSize = True
-        Me.decendingOrderCheckBox.Location = New System.Drawing.Point(9, 50)
-        Me.decendingOrderCheckBox.Name = "decendingOrderCheckBox"
-        Me.decendingOrderCheckBox.Size = New System.Drawing.Size(78, 17)
-        Me.decendingOrderCheckBox.TabIndex = 1
-        Me.decendingOrderCheckBox.Text = "Decending"
-        Me.decendingOrderCheckBox.UseVisualStyleBackColor = True
-        '
-        'readCustomersButton
-        '
-        Me.readCustomersButton.Location = New System.Drawing.Point(181, 23)
-        Me.readCustomersButton.Name = "readCustomersButton"
-        Me.readCustomersButton.Size = New System.Drawing.Size(75, 23)
-        Me.readCustomersButton.TabIndex = 2
-        Me.readCustomersButton.Text = "Read"
-        Me.readCustomersButton.UseVisualStyleBackColor = True
         '
         'GroupBox2
         '
@@ -125,21 +85,61 @@ Partial Class Form1
         Me.countriesComboBox.Size = New System.Drawing.Size(152, 21)
         Me.countriesComboBox.TabIndex = 0
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.readCustomersButton)
+        Me.GroupBox1.Controls.Add(Me.decendingOrderCheckBox)
+        Me.GroupBox1.Controls.Add(Me.columnNamesComboBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(9, 10)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(282, 84)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Order by"
+        '
+        'readCustomersButton
+        '
+        Me.readCustomersButton.Location = New System.Drawing.Point(181, 23)
+        Me.readCustomersButton.Name = "readCustomersButton"
+        Me.readCustomersButton.Size = New System.Drawing.Size(75, 23)
+        Me.readCustomersButton.TabIndex = 2
+        Me.readCustomersButton.Text = "Read"
+        Me.readCustomersButton.UseVisualStyleBackColor = True
+        '
+        'decendingOrderCheckBox
+        '
+        Me.decendingOrderCheckBox.AutoSize = True
+        Me.decendingOrderCheckBox.Location = New System.Drawing.Point(9, 50)
+        Me.decendingOrderCheckBox.Name = "decendingOrderCheckBox"
+        Me.decendingOrderCheckBox.Size = New System.Drawing.Size(78, 17)
+        Me.decendingOrderCheckBox.TabIndex = 1
+        Me.decendingOrderCheckBox.Text = "Decending"
+        Me.decendingOrderCheckBox.UseVisualStyleBackColor = True
+        '
+        'columnNamesComboBox
+        '
+        Me.columnNamesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.columnNamesComboBox.FormattingEnabled = True
+        Me.columnNamesComboBox.Location = New System.Drawing.Point(9, 23)
+        Me.columnNamesComboBox.Name = "columnNamesComboBox"
+        Me.columnNamesComboBox.Size = New System.Drawing.Size(152, 21)
+        Me.columnNamesComboBox.TabIndex = 0
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Builder patten sample"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
-        Me.GroupBox2.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
